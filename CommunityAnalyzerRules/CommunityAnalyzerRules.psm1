32fb72cda31c0724b196993bed234567fd591c06
Kriegel <peter.kriegel@mailbox.org>
2018-03-01 19:19:31 +0100

    Bugfix: Found only 1 result ; Changed ForEach-Object to ForEach Loop; code refactored

--- CommunityAnalyzerRules/CommunityAnalyzerRules.psm1
+++ CommunityAnalyzerRules/CommunityAnalyzerRules.psm1
@@ -3,11 +3,12 @@
 # Import Localized Data
 Import-LocalizedData -BindingVariable Messages
 
-Function New-PsScriptAnalyzerCorrectionExtent {
+Function New-PsScriptAnalyzerCorrectionExtent 
+{
     <#
-.SYNOPSIS
-    Function to Create a new CorrectionExtent Object for the PSScriptAnalyser result
-#>
+            .SYNOPSIS
+            Function to Create a new CorrectionExtent Object for the PSScriptAnalyser result
+    #>
 
     [CmdletBinding()]
     param(
@@ -33,34 +34,35 @@ Function New-PsScriptAnalyzerCorrectionExtent {
         $ReplacementText,
 
         [String]
-        $Path,
+        $Path = $Null,
 
         [String]
-        $Description
+        $Description = $Null
     )
 
     New-Object `
-        -TypeName 'Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent' `
-        -ArgumentList $startLineNumber, $endLineNumber, $startColumnNumber, $endColumnNumber, $replacementText, $path, $description
+    -TypeName 'Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent' `
+    -ArgumentList $StartLineNumber, $EndLineNumber, $StartColumnNumber, $EndColumnNumber, $ReplacementText, $Path, $Description
 }
 
-Function New-PsScriptAnalyzerDiagnosticRecord {
+Function New-PsScriptAnalyzerDiagnosticRecord 
+{
     <#
-.SYNOPSIS
-    Function to Create a new DiagnosticRecord Object for the PSScriptAnalyser result
-.PARAMETER message
-    A string about why this diagnostic was created
-.PARAMETER extent
-    The place in the script this diagnostic refers to
-.PARAMETER ruleName
-    The name of the rule that created this diagnostic
-.PARAMETER severity
-    The severity of this diagnostic
-.PARAMETER scriptPath
-    The full path of the script file being analyzed
-.PARAMETER suggestedCorrections
-    The correction suggested by the rule to replace the extent text
-#>
+            .SYNOPSIS
+            Function to Create a new DiagnosticRecord Object for the PSScriptAnalyser result
+            .PARAMETER message
+            A string about why this diagnostic was created
+            .PARAMETER extent
+            The place in the script this diagnostic refers to
+            .PARAMETER ruleName
+            The name of the rule that created this diagnostic
+            .PARAMETER severity
+            The severity of this diagnostic
+            .PARAMETER scriptPath
+            The full path of the script file being analyzed
+            .PARAMETER suggestedCorrections
+            The correction suggested by the rule to replace the extent text
+    #>
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $True)]
@@ -89,52 +91,59 @@ Function New-PsScriptAnalyzerDiagnosticRecord {
     )
 
     New-Object `
-        -TypeName 'Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord' `
-        -ArgumentList $Message, $Extent, $RuleName, $Severity, $ScriptPath, $null, $SuggestedCorrections
-
+    -TypeName 'Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord' `
+    -ArgumentList $Message, $Extent, $RuleName, $Severity, $ScriptPath, $Null, $SuggestedCorrections
 }
 
 <#
-.SYNOPSIS
-    Rule to detect positional parameters inside an CommandParameterAst
-.DESCRIPTION
-    Rule to detect positional parameters inside an CommandParameterAst
+        .SYNOPSIS
+        Rule to detect positional parameters inside an CommandParameterAst
+        .DESCRIPTION
+        Rule to detect positional parameters inside an CommandParameterAst
 
-    There are three case in which an Argument is considered as Positional
-    1. An argument follows direct to the CommandName
-    2. An argument follows direct to a SwitchParameter
-    3. An argument has no Parameter as direct precursor
+        There are three case in which an Argument is considered as Positional
+        1. An argument follows direct to the CommandName
+        2. An argument follows direct to a SwitchParameter
+        3. An argument has no Parameter as direct precursor
 
-    Get-Command is used to get the CmdletInfo Object from the command.
-    The Command must also be rocognized by Get-Command
-    The CmdletInfo Object is used to Resolve the parameter so we can detect switchparameter
+        Get-Command is used to get the CmdletInfo Object from the command.
+        The Command must also be rocognized by Get-Command
+        The CmdletInfo Object is used to Resolve the parameter so we can detect switchparameter
     
-    parameters with typos or wrong names, which cannot be resolved
-    are treated like an parameter which consumes an argument
-    Those parameter should be detecte by other rules! Also wrong typed commands.
+        parameters with typos or wrong names, which cannot be resolved
+        are treated like an parameter which consumes an argument
+        Those parameter should be detecte by other rules! Also wrong typed commands.
     
-    an argument which follows direct after an resolved or unresolved parameter is treated as bound to the parameter! 
+        an argument which follows direct after an resolved or unresolved parameter is treated as bound to the parameter! 
 
-.EXAMPLE
-    TODO: Measure-PositionalCommandParameter -ScriptBlockAst $ScriptBlockAst
-.INPUTS
-    [System.Management.Automation.Language.ScriptBlockAst]
-.OUTPUTS
-    [Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord[]]
-.NOTES
-    Author: Peter Kriegel
-    Version: 0.0.1
-    Date: 20.February.2018
-    History of changes:
+        .EXAMPLE
+        Measure-AvoidPositionalCommandParameter -ScriptBlockAst $ScriptBlockAst
+        .INPUTS
+        [System.Management.Automation.Language.ScriptBlockAst]
+        .OUTPUTS
+        [Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord[]]
+        .NOTES
+        Author: Peter Kriegel
+        Version: 0.0.1
+        Date: 20.February.2018
+        History of changes:
         V.0.0.1 Initial release
-#>
-function Measure-AvoidPositionalCommandParameter {
 
+        TODO:
+        Add exceptional rule for Select-Object, Where-Object and ForEach-Object !?
+        Select-Object can have calculatet Properties in a Hashtable
+        Where-Object has the new simple syntax.
+        ForEach-Object schould have a simple use wit curly brackets
+
+
+#>
+Function Measure-AvoidPositionalCommandParameter 
+{
     [CmdletBinding()]
     [OutputType([Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord[]])]
     Param
     (
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $True)]
         [ValidateNotNullOrEmpty()]
         [System.Management.Automation.Language.ScriptBlockAst]
         $ScriptBlockAst
@@ -145,92 +154,95 @@ function Measure-AvoidPositionalCommandParameter {
 
         $InvocationName = $PSCmdlet.MyInvocation.InvocationName
 
-        Function New-DiagnosticRecordInternal {
-
+        Function New-DiagnosticRecordInternal 
+        {
+            [CmdletBinding()]
             param(
                 $Extent
             )
 
             New-PsScriptAnalyzerDiagnosticRecord `
-                -Message $Messages.MeasurePositionalCommandParameter -Extent $Extent -RuleName $InvocationName -Severity 'Warning'
+            -Message "$($Messages.MeasurePositionalCommandParameter): $($Extent.Text)" -Extent $Extent -RuleName $InvocationName -Severity 'Warning'
         }
     }
 
     Process {
         $results = [System.Collections.ArrayList]@()
 
-        try {
-            #region Finds ASTs that match the predicates.
-            $ScriptBlockAst.FindAll( {$args[0] -is [System.Management.Automation.Language.CommandAst]}, $true) | ForEach-Object {
+        try 
+        {
+            $CommandAsts = $ScriptBlockAst.FindAll( {$args[0] -is [System.Management.Automation.Language.CommandAst]}, $True)
+           
+            Write-Debug -Message "--- CommandAst count $([Array]$CommandAsts.count) ---"
 
-                Write-Debug '--- Beginn to process a CommandAst ---'
+            ForEach($CurrendCmdAst in ([Array]$CommandAsts)) 
+            {
+                Write-Debug -Message "--- Beginn to process a CommandAst $($CurrendCmdAst.GetCommandName()) ---"
 
                 # getting rich informations about the command with Get-Command
-                $CmdletInfo = Get-Command -Name ($_.GetCommandName()) -ErrorAction SilentlyContinue
+                $CmdletInfo = Get-Command -Name ($CurrendCmdAst.GetCommandName()) -ErrorAction SilentlyContinue
 
                 # execute only if we have an CmdletInfo Object
-                If($Null -ne $CmdletInfo) {
+                If($Null -ne $CmdletInfo) 
+                {
+                    Write-Debug -Message '--- we have an CmdletInfo ---'
 
                     # process each element in CommandElements skipping element 0 which is the command himself
-                    for ($i = 1; $i -lt $_.CommandElements.count; $i++) {
-
-                        If ($_.CommandElements[$i] -isnot [System.Management.Automation.Language.CommandParameterAst]) {
-
-                            If ($i -gt 1 ) {
-
+                    for ($i = 1; $i -lt $CurrendCmdAst.CommandElements.count; $i++) 
+                    {
+                        If ($CurrendCmdAst.CommandElements[$i] -isnot [System.Management.Automation.Language.CommandParameterAst]) 
+                        {
+                            If ($i -gt 1 ) 
+                            {
                                 # test if the precursor element is an parameter and is NOT a switch parameter
-                                If ($_.CommandElements[$i - 1] -is [System.Management.Automation.Language.CommandParameterAst]) {
-
-                                    Try {
-                                        $ParameterMetadata = $CmdletInfo.ResolveParameter($_.CommandElements[$i - 1].Extent.Text)
+                                If ($CurrendCmdAst.CommandElements[$i - 1] -is [System.Management.Automation.Language.CommandParameterAst]) 
+                                {
+                                    Try 
+                                    {
+                                        $ParameterMetadata = $CmdletInfo.ResolveParameter($CurrendCmdAst.CommandElements[$i - 1].Extent.Text)
                                     }
-                                    Catch {
+                                    Catch 
+                                    {
                                         $ParameterMetadata = $Null
                                     }
 
-                                    If ($ParameterMetadata) {
-
-                                        If ($ParameterMetadata.ParameterType.Tostring() -eq 'System.Management.Automation.SwitchParameter') {
-
+                                    If ($ParameterMetadata) 
+                                    {
+                                        If ($ParameterMetadata.ParameterType.Tostring() -eq 'System.Management.Automation.SwitchParameter') 
+                                        {
                                             # Positional Argument after switch parameter found
-                                            Write-Debug "Argument $($_.CommandElements[$i].Extent.Text) is Positional"
+                                            Write-Debug -Message "Argument $($CurrendCmdAst.CommandElements[$i].Extent.Text) is Positional"
 
-                                            $result = New-DiagnosticRecordInternal -Extent $_.CommandElements[$i].Extent
+                                            $result = New-DiagnosticRecordInternal -Extent $CurrendCmdAst.CommandElements[$i].Extent
                                             $Null = $results.add($result)
-
                                         }
-
                                     }
-
                                 }
-                                Else {
-
+                                Else 
+                                {
                                     # Positional Argument without leading Parameter Found
-                                    Write-Debug "Argument $($_.CommandElements[$i].Extent.Text) is Positional"
-                                    $result = New-DiagnosticRecordInternal -Extent $_.CommandElements[$i].Extent
+                                    Write-Debug -Message "Argument $($CurrendCmdAst.CommandElements[$i].Extent.Text) is Positional"
+                                    $result = New-DiagnosticRecordInternal -Extent $CurrendCmdAst.CommandElements[$i].Extent
                                     $Null = $results.add($result)
                                 }
-
                             }
-                            Else {
-
+                            Else 
+                            {
                                 # Positional Argument direct after command on Position 0 Found
-                                Write-Debug "Argument $($_.CommandElements[$i].Extent.Text) is Positional"
-                                $result = New-DiagnosticRecordInternal -Extent $_.CommandElements[$i].Extent
+                                Write-Debug -Message "Argument $($CurrendCmdAst.CommandElements[$i].Extent.Text) is Positional"
+                                $result = New-DiagnosticRecordInternal -Extent $CurrendCmdAst.CommandElements[$i].Extent
                                 $Null = $results.add($result)
-
                             }
                         }
                     }
-                    Write-Debug '--- End to process a CommandAst ---'
+                    Write-Debug -Message "--- End to process a CommandAst $($CurrendCmdAst.GetCommandName()) ---"
                 }
-
-                return $results.ToArray()
             }
 
-            #endregion
+            return $results.ToArray()
         }
-        catch {
+        catch 
+        {
             $PSCmdlet.ThrowTerminatingError($PSItem)
         }
     }
